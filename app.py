481ecc1a7c833055069a0f2384ee63e61cfc86f5
Chad Smith <chadsmithmusic@icloud.com>
2021-10-08 16:08:24 -0700

    added secret.html

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, redirect, session, flash
-from flask_debugtoolbar import DebugToolbarExtension
+# from flask_debugtoolbar import DebugToolbarExtension
 from models import connect_db, User, db
 from forms import UserForm
 
@@ -12,13 +12,13 @@ app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 connect_db(app)
 
-toolbar = DebugToolbarExtension(app)
+# toolbar = DebugToolbarExtension(app)
 
 @app.route('/')
 def root():
     return redirect('/register')
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def create_user():
 
     form = UserForm()
@@ -28,9 +28,17 @@ def create_user():
         email = form.email.data
         first_name = form.first_name.data
         last_name = form.last_name.data
-        new_user = User.register(username, password, email, first_name, last_name)
 
-        db.session.add(new_user)
-        db.session.commit()
+        # new_user = User.register(username, password, email, first_name, last_name)
+        # db.session.add(new_user)
+        # db.session.commit()
+        return redirect('/secret')
 
     return render_template('register.html', form=form)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login_user():
+    form = UserForm()
+    if form.validate_on_submit():
+        username = form.username.data
+        password = form.password.data
