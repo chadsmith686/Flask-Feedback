8ea67f7206b1be97b3efe7ecbb91903848d9f7af
Chad Smith <chadsmithmusic@icloud.com>
2021-10-08 16:18:17 -0700

    added authenticate class method

--- models.py
+++ models.py
@@ -13,10 +13,36 @@ def connect_db(app):
     db.init_app(app)
 
 class User(db.Model):
-    __tablename__ = "users"
+    __tablename__ = 'user'
 
     username = db.Column(db.String(20), primary_key=True, unique=True, nullable=False)
     password = db.Column(db.Text, nullable=False)
     email = db.Column(db.String(50), nullable=False, unique=True)
     first_name = db.Column(db.String(30), nullable=False)
     last_name = db.Column(db.String(30), nullable=False)
+
+    @classmethod
+    def register(cls, username, pwd, email, first_name, last_name):
+        """Register user w/hashed password & return user."""
+
+        hashed = bcrypt.generate_password_hash(pwd)
+        # turn bytestring into normal (unicode utf8) string
+        hashed_utf8 = hashed.decode("utf8")
+
+        # return instance of user w/username and hashed pwd
+        return cls(username=username, password=hashed_utf8, email=email, first_name=first_name, last_name=last_name)
+
+    @classmethod
+    def authenticate(cls, username, pwd):
+        """Validate that user exists & password is correct.
+
+        Return user if valid; else return False.
+        """ 
+
+        user = User.query.filter_by(username=username).first()
+
+        if user and bcrypt.check_password_hash(user.password, pwd):
+            # return user instance
+            return user
+        else:
+            return False
